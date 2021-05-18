bf91b5d9bc37ab20af91c01e209b10f2435b2291
Daiki Chibana <a2018112376@estudiantes.upsa.edu.bo>
2021-05-17 23:29:16 -0400

    detalleInscripcion

--- NEGOCIO/detalleInscripcion.cs
+++ NEGOCIO/detalleInscripcion.cs
@@ -1,12 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NEGOCIO
+﻿namespace NEGOCIO
 {
-    class detalleInscripcion
+    using System;
+    using System.Data;
+    using DATOS;
+    public class detalleInscripcion : Conexion
     {
+        private int codInscripcion;
+        private int codApoderado;
+        private string relacion;
+
+        public detalleInscripcion()
+        {
+            codInscripcion = 0;
+            codApoderado = 0;
+            relacion = "";
+        }
+
+        public int IdcodInscripcion
+        {
+            get { return this.codInscripcion; }
+            set { this.codInscripcion = value; }
+        }
+
+        public int IdcodApoderado
+        {
+            get { return this.codApoderado; }
+            set { this.codApoderado = value; }
+        }
+
+        public string Relacion
+        {
+            get { return this.relacion; }
+            set { this.relacion = value; }
+        }
+
+
+
+        ///Metodos CRUD
+        public bool guardar()
+        {
+            iniciarSP("guardarEstudiante");
+            parametroInt(codInscripcion,"codInsc");
+            parametroInt(codApoderado,"codAp");
+            parametroVarchar(relacion, "rel", 30);
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+
+        public DataTable buscar()
+        {
+            iniciarSP("buscarDetalleInscripcion");
+            parametroInt(codInscripcion, "codInsc");
+            return mostrarData();
+        }
+
+        /*
+        public bool modificar()
+        {
+            iniciarSP("modificarDetalleInscripcion");
+            parametroInt(codInscripcion, "codInsc");
+            parametroInt(codApoderado, "codAp");
+            parametroVarchar(relacion, "rel", 30);
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+        */
+
+        public bool eliminar()
+        {
+            iniciarSP("eliminarDetalleInscripcion");
+            parametroInt(codInscripcion, "codInsc");
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+
+        public DataTable selectDetalleInscripcion()
+        {
+            iniciarSP("selectDetalleInscripcion");
+            return mostrarData();
+        }
     }
 }
