663fcd09a545cf22f1373b8848821dfa11905ae8
Miguel031 <53978289+Miguel031@users.noreply.github.com>
2021-05-17 21:58:44 -0400

    Modelo Inscripcion

--- NEGOCIO/Inscripcion.cs
+++ NEGOCIO/Inscripcion.cs
@@ -3,13 +3,80 @@
     using System;
     using System.Data;
     using DATOS;
+
     public class Inscripcion : Conexion
     {
 
+        private DateTime fec;
+        private int codcGestion;
+        private int codEst;
+        private int id_insc;
         public Inscripcion()
         {
-
+            id_insc = 0;  
+            fec = DateTime.Today.Date;
+            codcGestion = 0;
+            codEst = 0;
+        }
+        public int IdInscripcion
+        {
+            get { return this.id_insc; }
+            set { this.id_insc = value; }
+        }
+        public DateTime Fecha
+        {
+            get { return this.fec; }
+            set { this.fec = value; }
+        }
+        public int CodigoCursoGestion
+        {
+            get { return this.codcGestion; }
+            set { this.codcGestion = value; }
+        }
+        public int CodigoEstudiante
+        {
+            get { return this.codEst; }
+            set { this.codEst = value; }
         }
 
+        ///Metodos CRUD
+        public bool guardar()
+        {
+            iniciarSP("guardarInscripcion");
+            parametroInt(IdInscripcion, "id:insc");
+            parametroFecha(Fecha, "fec");
+            parametroInt(CodigoCursoGestion, "codcGestion");
+            parametroInt(CodigoEstudiante, "codEst");
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+
+        public DataTable buscar(string nomEst)
+        {
+            iniciarSP("buscarInscripcion");
+            parametroVarchar(nomEst, "buscar", 30);
+            return mostrarData();
+        }
+
+        public bool modificar()
+        {
+            iniciarSP("modificarInscripcion");
+            parametroInt(IdInscripcion, "id:insc");
+            parametroFecha(Fecha, "fec");
+            parametroInt(CodigoCursoGestion, "codcGestion");
+            parametroInt(CodigoEstudiante, "codEst");
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+        public bool eliminar()
+        {
+            iniciarSP("eliminarInscripcion");
+            parametroInt(IdInscripcion, "id_insc");
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+
+        public DataTable selectInscripcion()
+        {
+            iniciarSP("selectInscripcion");
+            return mostrarData();
+        }
     }
 }
